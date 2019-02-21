5d84cd60ade2cc6f409a3e963e6559059e5f870d
Maanas Pulganahalli M <maanas.pm@gmail.com>
2019-02-21 12:53:43 +0530

    commit

--- controller/logs/logs.go
+++ controller/logs/logs.go
@@ -23,14 +23,14 @@ func Routes() *chi.Mux {
 		m = make(map[int64]models.Log)
 	}
 	router := chi.NewRouter()
-	router.Get("/{todoID}", GetATodo)
-	router.Delete("/{todoID}", DeleteTodo)
-	router.Post("/", CreateTodo)
-	router.Get("/", GetAllTodos)
+	router.Get("/{todoID}", GetALog)
+	router.Delete("/{todoID}", DeleteLog)
+	router.Post("/", AddLog)
+	router.Get("/", GetAllLogs)
 	return router
 }
 
-func GetATodo(w http.ResponseWriter, r *http.Request) {
+func GetALog(w http.ResponseWriter, r *http.Request) {
 	todoID := chi.URLParam(r, "todoID")
 	todos := Todo{
 		Id:  todoID,
@@ -40,13 +40,13 @@ func GetATodo(w http.ResponseWriter, r *http.Request) {
 	render.JSON(w, r, todos) // A chi router helper for serializing and returning json
 }
 
-func DeleteTodo(w http.ResponseWriter, r *http.Request) {
+func DeleteLog(w http.ResponseWriter, r *http.Request) {
 	response := make(map[string]string)
 	response["message"] = "Deleted TODO successfully"
 	render.JSON(w, r, response) // Return some demo response
 }
 
-func CreateTodo(w http.ResponseWriter, r *http.Request) {
+func AddLog(w http.ResponseWriter, r *http.Request) {
 	req := r.Method + " " + r.Host + " "+ r.URL.Path
 	body, err := ioutil.ReadAll(r.Body)
     	if err != nil {
@@ -63,6 +63,10 @@ func CreateTodo(w http.ResponseWriter, r *http.Request) {
 	render.JSON(w, r, t) // Return some demo response
 }
 
-func GetAllTodos(w http.ResponseWriter, r *http.Request) {
-	render.JSON(w, r, m) // A chi router helper for serializing and returning json
+func GetAllLogs(w http.ResponseWriter, r *http.Request) {
+	var t [len(m)]models.Log
+	for k, v := range m { 
+		t = append(t,v)
+	}
+	render.JSON(w, r, t) // A chi router helper for serializing and returning json
 }
